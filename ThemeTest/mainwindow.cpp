0555e2bd63fcf1b73b318e8a83dfffea8a6ef4dd
Brigido Rodriguez <brigido.rodriquez@image-metrics.com>
2025-08-11 21:29:20 -0700

    Adding back the tableview test example.

--- ThemeTest/mainwindow.cpp
+++ ThemeTest/mainwindow.cpp
@@ -69,7 +69,11 @@ MainWindow::MainWindow(QWidget *parent) :
 {
     ui->setupUi(this);
 
-    // Table/list models removed since Views tab was removed
+    auto* table_model = new TableModel();
+    ui->tableView1->setModel(table_model);
+    ui->tableView2->setModel(table_model);
+    ui->listView1->setModel(table_model);
+    ui->listView2->setModel(table_model);
 
     // Populate dropdown with all available embedded themes
     for (const auto& themeInfo : themeInfos)
